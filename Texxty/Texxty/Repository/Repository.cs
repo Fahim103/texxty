4f7dd5c28452d890eea5099e3c5a357a6a493904
Ahmed Ali Fahim <fahimahmedali1@gmail.com>
2020-03-06 14:52:47 +0600

    Added Models from DB and Repository Class

---
+++ Texxty/Texxty/Repository/Repository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Texxty.Models;
+
+namespace Texxty.Repository
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        private readonly TexxtyDBEntities context;
+
+        public Repository()
+        {
+            this.context = new TexxtyDBEntities();
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return context.Set<TEntity>().ToList();
+        }
+
+        public TEntity Get(int id)
+        {
+            return context.Set<TEntity>().Find(id);
+        }
+
+        public void Insert(TEntity entity)
+        {
+            context.Set<TEntity>().Add(entity);
+            context.SaveChanges();
+        }
+
+        public void Update(TEntity entity)
+        {
+            try
+            {
+                context.Entry(entity).State = EntityState.Modified;
+                context.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                var msg = e.InnerException;
+            }
+
+        }
+
+        public void Delete(int id)
+        {
+            context.Set<TEntity>().Remove(this.Get(id));
+            context.SaveChanges();
+        }
+    }
+}
