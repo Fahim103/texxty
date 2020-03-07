341dc9db6153138fb7a95472573533e9ce7dda25
Ahmed Ali Fahim <fahimahmedali1@gmail.com>
2020-03-07 14:07:15 +0600

    Registration and Login done
    TODO: Need to add feature of activating account after email verification

--- Texxty/Texxty/Controllers/AccountsController.cs
+++ Texxty/Texxty/Controllers/AccountsController.cs
@@ -11,10 +11,12 @@ namespace Texxty.Controllers
     public class AccountsController : Controller
     {
         private readonly UserRepository userRepository;
+        private readonly TexxtyDBEntities context;
 
         public AccountsController()
         {
             userRepository = new UserRepository();
+            context = new TexxtyDBEntities();
         }
 
         [HttpGet]
@@ -33,20 +35,71 @@ namespace Texxty.Controllers
                     return View();
                 }
 
-                // TODO: Insert to DB
                 user.ActiveStatus = false; // Will set it to true when email is verified
-                userRepository.Insert(user)
+                userRepository.Insert(user);
 
                 // TODO: Send email to user
 
-
                 // Set success message
                 TempData["RegistrationMessage"] = "Account created. Please verify your email to activate your account.";
+                return RedirectToAction("Login");
 
             }
             TempData["RegistrationMessage"] = "Error. Please check your information again";
             return View();
         }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(User user)
+        {
+            // If ModelState is not valid
+            if (!ModelState.IsValid)
+                return View();
+
+            // Model State Valid
+            try
+            {
+                var logged_user = context.Users
+                   .Where(x => x.Username == user.Username && x.Password == user.Password)
+                   .FirstOrDefault();
+
+                // If credentials are correct
+                if (logged_user != null)
+                {
+                    // If account is active
+                    if(logged_user.ActiveStatus == true)
+                    {
+                        Session["username"] = logged_user.Username;
+                        Session["user_id"] = logged_user.UserID;
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        ViewBag.Message = "Your account is not active. Please verify your email.";
+                        return View();
+                    }
+                    
+                }
+                else
+                {
+                    ViewBag.Message = "Incorrect credentials.";
+                    return View();
+                }
+            }
+            catch
+            {
+                ViewBag.Message = "Failed to log in.";
+                return View();
+            }
+        }
+
+
 
         [NonAction]
         public bool ValidateRegister(User user, string Password2)
@@ -56,29 +109,25 @@ namespace Texxty.Controllers
                 TempData["RegistrationMessage"] = "Password doesn't match. Please try again";
                 return false;
             }
-            if (userRepository.CheckUsernameAvailable(user))
+            
+            if (userRepository.CheckEmailAvailable(user))
             {
-                if (userRepository.CheckEmailAvailable(user))
+                
+                if (userRepository.CheckUsernameAvailable(user))
                 {
                     return true;
                 }
                 else
                 {
-                    TempData["RegistrationMessage"] = "Account exists with this email";
+                    TempData["RegistrationMessage"] = "Username not available";
                     return false;
                 }
             }
             else
             {
-                TempData["RegistrationMessage"] = "Username not available";
+                TempData["RegistrationMessage"] = "Account exists with this email";
                 return false;
             }
-        }
-
-        [HttpGet]
-        public ActionResult Login()
-        {
-            return View();
         }
     }
 }
---
+++ Texxty/Texxty/Repository/Classes/UserRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Texxty.Models;
+
+namespace Texxty.Repository.Classes
+{
+    public class UserRepository : Repository<User>
+    {
+        private readonly TexxtyDBEntities context;
+
+        public UserRepository()
+        {
+            this.context = new TexxtyDBEntities();
+        }
+
+        public bool CheckUsernameAvailable(User user)
+        {
+            return (context.Users.Where(x => x.Username == user.Username).FirstOrDefault() == null);
+        }
+
+        public bool CheckEmailAvailable(User user)
+        {
+            return (context.Users.Where(x => x.Email == user.Email).FirstOrDefault() == null);
+        }
+    }
+}
