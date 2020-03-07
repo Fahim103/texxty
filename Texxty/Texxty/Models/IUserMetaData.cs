daa9bdfc11d3d447e941005d5a57019451da598a
Ahmed Ali Fahim <fahimahmedali1@gmail.com>
2020-03-07 13:10:07 +0600

    Added Metadata class for User table

---
+++ Texxty/Texxty/Models/IUserMetaData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Texxty.Models
+{
+    /// <summary>
+    /// Metadata interface for User table
+    /// </summary>
+    public interface IUserMetaData
+    {
+        int UserID { get; set; }
+
+        [Required(ErrorMessage ="Username is required")]
+        string Username { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        string Email { get; set; }
+        
+        [Required(ErrorMessage = "Password is required")]
+        string Password { get; set; }
+
+        [Required(ErrorMessage = "Fullname is required")]
+        string FullName { get; set; }
+
+        bool ActiveStatus { get; set; }
+    }
+}
