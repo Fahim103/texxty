191c39862ae58ba35822003a58aa6d8d09da4754
Ahmed Ali Fahim <fahimahmedali1@gmail.com>
2020-04-04 18:30:45 +0600

    Basic Authentication
    Added Authentication Filter Class
    Login with authentication token (Base64 Format)

---
+++ Texxty/Texxty-api/Controllers/AccountsController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Web.Http;
+using Texxty_api.Attributes;
+using Texxty_api.Models;
+using Texxty_api.Models.Utilities;
+using Texxty_api.Repository.Classes;
+
+namespace Texxty_api.Controllers
+{
+    public class AccountsController : ApiController
+    {
+        [BasicAuthentication]
+        // TEST METHOD 
+        public IHttpActionResult GetUserInfo()
+        {
+            string username = Thread.CurrentPrincipal.Identity.Name;
+
+            if (username != null)
+                return Ok(username + "-> GetUserInfo");
+            else
+                return Ok("ERROR");
+        }
+
+        [HttpPost]
+        public IHttpActionResult Login()
+        {
+            if (Request.Headers.Authorization == null)
+                return StatusCode(HttpStatusCode.Unauthorized);
+
+            // Get authenticationToken from the Request Header
+            string authenticationToken = Request.Headers.Authorization.Parameter;
+
+            if (AuthenticationUtility.AuthenticateUser(authenticationToken))
+            {
+                return Ok("Login Successful");
+            }
+            else
+            {
+                return StatusCode(HttpStatusCode.Unauthorized);
+            }
+        }
+    }
+}
