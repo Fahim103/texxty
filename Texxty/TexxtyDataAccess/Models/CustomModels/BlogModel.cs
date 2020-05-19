4c0c8b82a2ce2de4127fcf2cd31d4bdbf512f02e
sazid <sazidozon@gmail.com>
2020-05-20 05:48:48 +0600

    HATEOAS for Blog model

--- Texxty/TexxtyDataAccess/Models/CustomModels/BlogModel.cs
+++ Texxty/TexxtyDataAccess/Models/CustomModels/BlogModel.cs
@@ -15,5 +15,17 @@ namespace TexxtyDataAccess.Models.CustomModels
         public int UserID { get; set; }
         public int TopicID { get; set; }
         public int ViewCount { get; set; }
+
+        public List<string> Resources = new List<string>
+            {
+                "[GET]      api/Blogs",
+                "[GET]      api/Users/{user_id}/Blogs",
+                "[GET]      api/Users/{user_id}/Blogs/{blog_id}",
+                "[GET]      api/Blogs/{blog_id}",
+                "[POST]     api/Blogs",
+                "[PUT]      api/Blogs/{blog_id}",
+                "[PUT]      api/Blogs/{blog_id}/UpdateViewCount",
+                "[DELETE]   api/Blogs/{blog_id}",
+            };
     }
 }
